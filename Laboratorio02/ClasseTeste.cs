651ed1f57d38e52fe9ac305895b4a7bfda817014
Davi <davidiasdasilva21@gmail.com>
2021-03-24 00:08:06 -0300

    Adicionar arquivos de projeto.

---
+++ Laboratorio02/ClasseTeste.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Laboratorio02
+{
+    class ClasseTeste
+    {
+        static void Main(string[] args)
+        {
+            Conta[] registroContas = new Conta[3];
+
+            registroContas[0] = new Conta(123456, "Bob Nelson", true);
+            registroContas[1] = new Conta(717171, "Testolfo Rocha", false);
+            registroContas[2] = new Conta(654321, "Lisa Leite", true);
+
+            registroContas[0].DepositarDinheiro(5000f);
+            registroContas[2].DepositarDinheiro(2000f);
+            registroContas[1].DepositarDinheiro(1500f);
+            registroContas[0].TransferirDinheiro(600f, 717171, registroContas);
+            registroContas[2].SacarDinheiro(250f);
+            registroContas[2].TransferirDinheiro(400f, 717171, registroContas);
+            registroContas[1].TransferirDinheiro(1000f, 123456, registroContas);
+            registroContas[0].SacarDinheiro(900f);
+            registroContas[0].TransferirDinheiro(1500f, 654321, registroContas);
+            registroContas[1].TransferirDinheiro(1200f, 654321, registroContas);
+            registroContas[0].SacarDinheiro(2000f);
+            registroContas[2].DepositarDinheiro(100f);
+            registroContas[1].TransferirDinheiro(700f, 123456, registroContas);
+
+            Console.WriteLine();
+            Console.WriteLine("RESULTADOS FINAIS:");
+            foreach(Conta conta in registroContas)
+            {
+                Console.WriteLine("Saldo de " + conta.NomeCorrentista + " : R$" + conta.VerificarSaldo());
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Laboratorio02/Conta.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio02
+{
+    class Conta
+    {
+        private int numIdentificacao;
+        private String nomeCorrentista;
+        private float saldo;
+        private bool contaCorrente;
+
+        public int NumIdentificacao { get => numIdentificacao; }
+        public string NomeCorrentista { get => nomeCorrentista; }
+
+        public Conta(int numIdentificacao_, String nomeCorrentista_, bool contaCorrente_)
+        {
+            this.numIdentificacao = numIdentificacao_;
+            this.nomeCorrentista = nomeCorrentista_;
+            this.saldo = 0;
+            this.contaCorrente = contaCorrente_;
+        }
+
+        public void SacarDinheiro(float valorParaSacar)
+        {
+            float valorComTarifa;
+
+            if (contaCorrente)
+            {
+                valorComTarifa = valorParaSacar * 1.0037f;
+            }
+            else
+            {
+                valorComTarifa = valorParaSacar * 1.002f;
+            }
+
+            if ((VerificarSaldo() - valorComTarifa) < 0)
+            {
+                Console.WriteLine("Erro. Saque da conta de " + this.nomeCorrentista + " no valor de R$ " + valorParaSacar + " é impossível. Saldo insuficiente.");
+                Console.WriteLine("Operação cancelada.");
+            }
+            else
+            {
+                saldo -= valorComTarifa;
+            }
+        }
+
+        public void DepositarDinheiro(float valorParaDepositar)
+        {
+            saldo += valorParaDepositar;
+            Console.WriteLine("Depósito na conta de " + this.nomeCorrentista + " no valor de R$ " + valorParaDepositar + " bem sucedido.");
+        }
+
+        public void TransferirDinheiro(float valorParaTransferir, int idContaDestino, Conta[] contasRegistradas)
+        {
+            float valorComTarifa;
+
+            if (contaCorrente)
+            {
+                valorComTarifa = valorParaTransferir * 1.001f;
+            }
+            else
+            {
+                valorComTarifa = valorParaTransferir * 1.0015f;
+            }
+
+            if ((VerificarSaldo() - valorComTarifa) < 0)
+            {
+                Console.WriteLine("Erro. Transferência de R$" + valorParaTransferir + " é impossível. Saldo insuficiente.");
+                Console.WriteLine("Operação cancelada.");
+                return;
+            }
+            else
+            {
+                saldo -= valorComTarifa;
+            }
+
+            foreach (Conta contaRegistrada in contasRegistradas)
+            {
+                if (idContaDestino == contaRegistrada.NumIdentificacao)
+                {
+                    contaRegistrada.DepositarDinheiro(valorParaTransferir);
+                    Console.WriteLine("Transferência de " + this.nomeCorrentista + " para " + contaRegistrada.NomeCorrentista + " no valor de R$ " + valorParaTransferir + " bem sucedida!");
+                    break;
+                }
+            }
+        }
+
+        public float VerificarSaldo()
+        {
+            return this.saldo;
+        }
+    }
+}
